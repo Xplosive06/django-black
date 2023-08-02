633e07c0c2691592ac10a912b633cd30181d341f
Mike Notta <nottamike92@gmail.com>
2023-08-02 14:52:43 +0200

    trying to fix 403 forbidden

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,12 @@ DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web-production-268b.up.railway.app', 'web-production-268b.up.railway.app/search/', "https://web-production-268b.up.railway.app"]
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'web-production-268b.up.railway.app', 'web-production-268b.up.railway.app/search/', "https://web-production-268b.up.railway.app"]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+    'web-production-268b.up.railway.app', 'web-production-268b.up.railway.app/search/', "https://web-production-268b.up.railway.app"
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
